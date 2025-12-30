97fe5013aec9e1c1e885089e7d655e6fa99df49e
Amber Lee <amber_Lee1993@hotmail.com>
2025-12-29 17:16:04 -0800

    day 11 part 2

--- src/day11/day11.ts
+++ src/day11/day11.ts
@@ -22,7 +22,7 @@ function createPathKey(path: string[]) {
   return path.join("-");
 }
 
-function bfsSearch(nodes: Node[], nodeMap: Map<string, string[]>) {
+function bfsSearch(nodeMap: Map<string, string[]>) {
   const seen: Set<string> = new Set();
   const queue = ["you"];
   let numPaths = 0;
@@ -51,7 +51,41 @@ function part1() {
   for (const node of nodes) {
     nodeMap.set(node.from, node.to);
   }
-  return bfsSearch(nodes, nodeMap);
+  return bfsSearch(nodeMap);
+}
+const memo = new Map<string, number>();
+function dfs(
+  node: string,
+  dac: boolean,
+  fft: boolean,
+  nodeMap: Map<string, string[]>
+): number {
+  const key = `${node}-${dac ? 1 : 0}-${fft ? 1 : 0}`;
+  if (memo.has(key)) return memo.get(key)!;
+  if (node === "out" && fft && dac) {
+    return 1;
+  }
+  if (node === "dac") {
+    dac = true;
+  }
+  if (node === "fft") {
+    fft = true;
+  }
+  let total = 0;
+  for (const neighbor of nodeMap.get(node) || []) {
+    total += dfs(neighbor, dac, fft, nodeMap);
+  }
+  memo.set(key, total);
+  return total;
+}
+function part2() {
+  const nodes = readInput();
+  const nodeMap = new Map();
+  for (const node of nodes) {
+    nodeMap.set(node.from, node.to);
+  }
+  return dfs("svr", false, false, nodeMap);
 }
 
 console.log("Part 1: ", part1());
+console.log("Part 2: ", part2());
